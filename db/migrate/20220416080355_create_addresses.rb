8a6ac5b12623ef40f96e737ce6248c979fb32f85
sakamoto-waka <wakanmhl@gmail.com>
2022-04-16 08:06:04 +0000

    [add] columns to Customer, Address

---
+++ db/migrate/20220416080355_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.references :customer, null: false, foreign_key: true
+      t.string :name
+      t.string :postal_code
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
