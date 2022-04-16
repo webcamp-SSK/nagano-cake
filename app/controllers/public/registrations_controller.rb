2031b2995e244961f70ca56b117c34ba5beb816c
sakamoto-waka <wakanmhl@gmail.com>
2022-04-16 13:46:28 +0000

    [update] all forms done but no bootstrap

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -41,15 +41,15 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,
-                                                       :last_name_kana,
-                                                       :first_name,
-                                                       :first_name_kana,
-                                                       :postal_code,
-                                                       :address,
-                                                       :telephone_number])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,
+                                                         :last_name_kana,
+                                                         :first_name,
+                                                         :first_name_kana,
+                                                         :postal_code,
+                                                         :address,
+                                                         :telephone_number])
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
