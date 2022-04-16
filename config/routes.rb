0c4e289a37fd678570edfebba91c99cd4f579612
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-16 05:52:59 +0000

    [add] public items_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
   end
