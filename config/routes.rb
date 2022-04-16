4e4062c9f20979be85d9d66e25eb6f81aab5efe0
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-16 07:30:55 +0000

    [add] admin genres_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
     get 'orders/index'
     get 'orders/show'
   end
