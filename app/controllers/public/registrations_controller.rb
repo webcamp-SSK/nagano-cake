d8e96c33f4d8571b94f40f28266141ea3ba73bc3
sakamoto-waka <wakanmhl@gmail.com>
2022-04-17 12:29:34 +0000

    [update] contorollers to add no login no access execpt: homes, items

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -51,16 +51,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
                                                          :telephone_number])
     end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   public_customers_show_path(resource)
-  # end
-
   def after_sign_up_path_for(resource)
     my_page_customers_path(resource)
   end
@@ -68,12 +58,5 @@ class Public::RegistrationsController < Devise::RegistrationsController
   def after_sign_out_path_for(resource)
     public_homes_top_path
   end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-
 
 end
