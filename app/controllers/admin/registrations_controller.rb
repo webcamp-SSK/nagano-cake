6e26347e47d0834ddac07cbd1e4800273c1106e5
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-17 00:14:17 +0000

    [add]after signoutpath

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -54,6 +54,10 @@ class Admin::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     admin_homes_top_path
   end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
