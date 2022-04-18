b675894490af28b630e3bfcf0fb2fe8cab872bcc
ShunKato0625 <shunkato0625@gmail.com>
2022-04-18 05:05:26 +0000

    [Update] item view & controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  has_many :order_details, dependent: :destroy
+  has_many :orders, through: :order_details
+  has_many :cart_items, dependent: :destroy
+  has_many :customers, through: :cart_items
   belongs_to :genre
 
   with_options presence: true do
