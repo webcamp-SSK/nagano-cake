5db1c0bc460abed28954b2adeac8016e21abaec0
sakamoto-waka <wakanmhl@gmail.com>
2022-04-18 02:00:50 +0000

    [add] customers#withdraw

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,7 +14,13 @@ class Public::CustomersController < ApplicationController
   end
   
   def unsubscribe
-    
+  end
+  
+  def withdraw
+    current_customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました"
+    redirect_to root_path
   end
   
 end
