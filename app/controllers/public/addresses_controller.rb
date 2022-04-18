49bb27395c51a3ff1d09851bb4a771f7c8bad9b0
sakamoto-waka <wakanmhl@gmail.com>
2022-04-18 04:04:54 +0000

    [add] addresses/_address_form partial file

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,12 +2,17 @@ class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
 
   def index
+    @addresses = current_customer.address
+    @address = Address.new
+    
   end
 
   def edit
   end
 
   def create
+    @address = current_customer.address.new(address_params)
+    @address.save
   end
 
   def update
