e8c3081a11de7208a28731da2c3c80158d096a9e
sakamoto-waka <wakanmhl@gmail.com>
2022-04-18 10:54:54 +0000

    [fix] pub/addresses/edit form routing error

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -7,20 +7,29 @@ class Public::AddressesController < ApplicationController
 
   end
 
-  def edit
-    @address = Address.find(params[:id])
-  end
 
   def create
     @address = current_customer.addresses.new(address_params)
     if @address.save
       redirect_to request.referer
     else
+      @addresses = current_customer.addresses
       render :index
     end
   end
 
+  def edit
+    @address = Address.find(params[:id])
+  end
+
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+
   end
 
   def destroy
