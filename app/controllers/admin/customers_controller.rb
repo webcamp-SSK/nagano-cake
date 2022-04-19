3f6a9a9cc490eade50bb56ad307045253b9371ca
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-19 00:45:37 +0000

    [update]admin customer edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,5 +14,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render "edit"
+    end
   end
+
+  private
+
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+                                       :postal_code, :address, :telephone_number, :email, :is_deleted)
+    end
 end
