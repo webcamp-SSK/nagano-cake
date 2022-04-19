a3a90cba2cb1fe7c8ce0b58088b4d353e4c255d9
sakamoto-waka <wakanmhl@gmail.com>
2022-04-19 10:48:09 +0000

    [これから質問する]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,9 +5,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    if current_customer.update(customer_params)
+      redirect_to my_page_path
+    else
+      @customer = current_customer
+      render 'public/customers/edit'
+    end
   end
 
   def destroy
@@ -22,5 +29,12 @@ class Public::CustomersController < ApplicationController
     flash[:notice] = "退会処理を実行しました"
     redirect_to root_path
   end
+
+  private
+
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+                                       :postal_code, :address, :telephone_number, :email)
+    end
 
 end
