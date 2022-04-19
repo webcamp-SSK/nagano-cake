8bc069deb92d6e1a3dd9b11cb496ea902832bfb2
ShunKato0625 <shunkato0625@gmail.com>
2022-04-19 03:22:06 +0000

    [save]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
     @items = Item.where(is_active: 1).page(params[:page]).per(8)
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
