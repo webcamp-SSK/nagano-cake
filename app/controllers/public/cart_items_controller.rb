9f138e3fe79784a694fcbac98f16bd2daee6b609
sakamoto-waka <wakanmhl@gmail.com>
2022-04-19 02:30:49 +0000

    [update] pub/cart_items/index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,6 +6,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      @item = Item.find(params[:item_id])
+      render template: 'public/items/show'
+    end
   end
 
   def update
@@ -16,4 +24,11 @@ class Public::CartItemsController < ApplicationController
 
   def all_destroy
   end
+
+  private
+
+    def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+    end
+
 end
