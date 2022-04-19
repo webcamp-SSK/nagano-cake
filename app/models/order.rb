76b8853d8fea57c5cca349fae415d1fc3fe32dad
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-19 12:59:29 +0000

    動作確認

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,6 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting: 0, confirm: 1, making: 2, preparation: 3, sent: 4 }
 
 end
