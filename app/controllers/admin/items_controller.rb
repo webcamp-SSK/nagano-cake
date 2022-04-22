b4185f3acff0769f89a49f435b7ce50f66c3920a
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-22 01:37:13 +0000

    途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,6 +20,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
+      flash[:notice] = "商品を登録しました。"
       redirect_to admin_item_path(@item.id)
     else
       render "new"
@@ -29,6 +30,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
+      flash[:notice] = "商品を編集しました。"
       redirect_to admin_item_path(@item.id)
     else
       render "edit"
