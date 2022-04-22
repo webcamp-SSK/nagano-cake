69156197180b4ed93d0e3d5555b1a2dee01e5563
sakamoto-waka <wakanmhl@gmail.com>
2022-04-22 13:44:06 +0000

    [update] success to search by Order status

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,11 @@
 class Admin::HomesController < ApplicationController
   def top
     if params[:status]
-      @orders = Order.where(params[:status]).page(params[:page]).per(10)
+      @status = params[:status]
+      @order_status = Order.where(status: @status)
+      # @order_status.status_i18n
+      # debugger
+      @orders = @order_status.page(params[:page]).per(10)
     else
       @orders = Order.all.page(params[:page]).per(10)
     end
