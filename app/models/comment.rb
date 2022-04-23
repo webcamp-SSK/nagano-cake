2b803ffcea51a1c0d88d716a2561fa42d750fda7
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-23 10:51:45 +0000

    [update]raty

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
 
   belongs_to :customer
   belongs_to :item
+  default_scope -> {order(created_at: :desc)}
 
   with_options presence: true do
     validates :customer_id
