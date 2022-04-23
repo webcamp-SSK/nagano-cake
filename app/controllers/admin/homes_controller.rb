e587983f09da4bd4ec4150be798bb45b51b93b06
sakamoto-waka <wakanmhl@gmail.com>
2022-04-23 02:48:59 +0000

    [update] admin/items/index table-hover link to <tr>

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,13 +1,16 @@
 class Admin::HomesController < ApplicationController
   def top
     if params[:status]
-      @status = params[:status]
-      @order_status = Order.where(status: @status)
-      # @order_status.status_i18n
-      # debugger
-      @orders = @order_status.page(params[:page]).per(10)
+      @order_statuses = Order.where(status: params[:status])
+      @orders = @order_statuses.page(params[:page]).per(10)
     else
       @orders = Order.all.page(params[:page]).per(10)
     end
   end
+  
+  private
+    def order_status_params
+      params.require(:order).permit(:status)
+    end
+  
 end
