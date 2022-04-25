5730ada5fa2c3330761b252c67a86b8f59f70718
shigetomoshigetomo <runorunonyanmyu@gmail.com>
2022-04-25 13:10:17 +0000

    [fixed]search controller

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,6 @@
 class Public::SearchesController < ApplicationController
 	def search
-      @content=params[:content]
-      @records=Item.search_for(@content).page(params[:page]).per(6)
+    @content=params[:content]
+    @records=Item.where(is_active: 1).search_for(@content).page(params[:page]).per(6)
   end
 end
